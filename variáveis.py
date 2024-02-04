fdd28f683d070fd9efc4d748e6c466351c4b1b77
Mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2024-02-04 10:35:22 -0300

    Create variáveis.py

---
+++ variáveis.py
@@ -0,0 +1,12 @@
+#variáveis
+
+nome=str()
+idade=int()
+sexo=str()
+
+nome=input("Digite o seu nome:")
+idade=input("Digite a sua idade:")
+sexo=input("Digite o seu sexo:")
+
+print("Olá", nome, "bem-vindo",)
+print("Sabemos que você tem", idade, "anos," " e você é do sexo", sexo)
